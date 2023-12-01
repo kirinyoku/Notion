56c3d21252a1aa958cab67a94352ad5786cdcea9
KIRIN <103432491+kirinyoku@users.noreply.github.com>
2023-12-01 14:02:56 +0200

    Added a banner for deleting/restoring a page

--- src/app/(main)/_components/Navbar.tsx
+++ src/app/(main)/_components/Navbar.tsx
@@ -6,6 +6,8 @@ import { api } from "../../../../convex/_generated/api";
 import { Id } from "../../../../convex/_generated/dataModel";
 import { MenuIcon } from "lucide-react";
 import Title from "./Title";
+import Banner from "./Banner";
+import Menu from "./Menu";
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -20,8 +22,11 @@ export default function Navbar({ isCollapsed, onResetWidth }: NavbarProps) {
 
   if (document === undefined) {
     return (
-      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center">
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between">
         <Title.Skeleton />
+        <div className="flex-items-center gap-x-2">
+          <Menu.Skeleton />
+        </div>
       </nav>
     );
   }
@@ -42,8 +47,12 @@ export default function Navbar({ isCollapsed, onResetWidth }: NavbarProps) {
         )}
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
+          <div className="flex items-center gap-x-2">
+            <Menu documentId={document._id} />
+          </div>
         </div>
       </nav>
+      {document.isArchived && <Banner documentId={document._id} />}
     </>
   );
 }
